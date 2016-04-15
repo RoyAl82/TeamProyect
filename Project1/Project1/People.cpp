7670ff3519a92611b2725ab52c8c58e52e6a1018
Roicxy Alonso <Roicxy.Alonso001@mymdc.net>
2016-04-15 13:21:52 -0400

    I created a People class with two variables name and lastname

---
+++ Project1/Project1/People.cpp
@@ -0,0 +1,14 @@
+#include "People.h"
+
+
+
+People::People()
+{
+	name = "Not name provided.";
+	lastname = "Not lastname provided.";
+}
+
+People::~People()
+{
+
+}
---
+++ Project1/Project1/People.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+
+ref class People
+{
+public:
+	People();
+	~People();
+
+private:
+	String name;
+	String lastname;
+
+};
+
