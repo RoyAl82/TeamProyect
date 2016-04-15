69d39945477abb620d3a38ca26b316d5b1c126ba
Roicxy Alonso <Roicxy.Alonso001@mymdc.net>
2016-04-15 13:55:06 -0400

    I have created a UI form. its name is LogForm.h

---
+++ Project1/Project1/LogForm.cpp
@@ -0,0 +1,16 @@
+#include "LogForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Project1::LogForm form;
+	Application::Run(%form);
+}
